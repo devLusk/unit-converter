30992877701bbeb52b314dd8130a7da47cb0f40f
Lucas Silva <lucasvida2005@gmail.com>
2025-05-13 11:35:46 -0300

    more modularization

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,9 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,6 +26,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import com.example.unitconverter.utils.InputField
+import com.example.unitconverter.utils.ResultField
 import com.example.unitconverter.utils.UnitDropdownMenu
 
 class MainActivity : ComponentActivity() {
@@ -67,11 +64,10 @@ fun UnitConverter(modifier: Modifier = Modifier) {
 
             Spacer(modifier = Modifier.height(24.dp))
 
-            OutlinedTextField(
+            InputField(
                 value = inputValue,
-                onValueChange = { inputValue = it },
-                label = { Text("Enter the value") },
-                modifier = Modifier.fillMaxWidth()
+                onChange = { inputValue = it },
+                label = "Enter the value"
             )
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -97,20 +93,7 @@ fun UnitConverter(modifier: Modifier = Modifier) {
 
             Spacer(modifier = Modifier.height(24.dp))
 
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = MaterialTheme.colorScheme.surfaceVariant,
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .padding(15.dp)
-            ) {
-                Text(
-                    text = "Result: ",
-                )
-            }
-
+            ResultField()
         }
     }
 }
